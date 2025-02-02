891e1d32c4d676f9ff5cf5bc47aa92e1acb878d8
shifta-robel <robelbire16@gmail.com>
2025-02-02 17:55:13 +0300

    [FEAT] vector_store: added an in memory vec store
    	- cleaned up the `VectorStore` trait
    	- added an in memory vector store

--- core/src/vector_store/mod.rs
+++ core/src/vector_store/mod.rs
@@ -1,20 +1,35 @@
 use async_trait::async_trait;
-use uuid::Uuid;
 
 use super::embeddings::embedding::Embedding;
 
-#[derive(Debug)]
+mod in_memory_vec_store;
+pub use in_memory_vec_store::InMemoryVectorStore;
+
+#[derive(Debug, PartialEq, Eq)]
 pub enum VectorStoreError {
     Undefined,
+    EmbeddingNotFound,
 }
 
 #[async_trait]
 pub trait VectorStore: Send + Sync {
-    async fn get_by_id(&self, id: Uuid) -> Result<Option<Embedding>, VectorStoreError>;
+    /// Fetch an Embedding from the vec store with a matching id
+    async fn get_by_id(&self, id: String) -> Result<Embedding, VectorStoreError>;
 
-    async fn store(&self, data: Embedding) -> Result<(), VectorStoreError>;
+    /// Stores or updates an embedding in the vector store
+    ///
+    /// if the `raw_data` field is empty, it removes the embedding from the store.
+    /// if the `raw_data` fiels is empty and the embedding does not exist, it returns an `EmbeddingNotFound`.
+    /// if the `raw_data` is not empty, it inserts or updates the embedding in the store
+    async fn store(&self, embedding: Embedding) -> Result<(), VectorStoreError>;
 
+    /// Fetch top n `Embedding`s ordered by cosine_similarity score
     async fn top_n(&self, query: &[f64], n: usize) -> Result<Vec<Embedding>, VectorStoreError>;
+}
 
-    async fn has(&self, id: Uuid) -> Result<bool, VectorStoreError>;
+pub(crate) fn cosine_similarity(a: &[f64], b: &[f64]) -> f64 {
+    let dot_product: f64 = a.iter().zip(b).map(|(x, y)| x * y).sum();
+    let norm_a = a.iter().map(|x| x * x).sum::<f64>().sqrt();
+    let norm_b = b.iter().map(|x| x * x).sum::<f64>().sqrt();
+    dot_product / (norm_a * norm_b)
 }
